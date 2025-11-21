8a04e23e6d58f6c686ffdcef4639cecd301c2c56
SiJibrilll <jibrilianwa@gmail.com>
2025-11-21 19:06:45 +0700

    config for termux

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        watch: {
+            usePolling: true,
+            interval: 200,
+            ignored: ['**/vendor/**'],
+        },
+        hmr: {
+            host: 'localhost',
+        },
+    },
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -9,3 +20,4 @@ export default defineConfig({
         }),
     ],
 });
+
